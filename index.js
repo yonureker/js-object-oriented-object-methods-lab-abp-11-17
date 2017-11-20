289a9982dd0250cce79169f0b7c2bbeb94e23d8c
Onur Eker <yonureker@gmail.com>
2017-11-20 22:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function BoardMember (name, homeState, training){
   this.homeState = homeState
   this.training = training
   this.veto = function (){
-    return "No I must disagree"
+    return "No, I must disagree"
   }
   this.approve = function(){
     return "You can do that!"
   }
   this.doCharity = function (){
-    return "I like to help people"
+    return "I like to help people."
   }
   this.releasePressStatement = function(){
-    return "You will see great things from Scuber"
+    return "You will see great things from Scuber."
   }
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`
